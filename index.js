c255ba36ea93f27feb5041fb0046eeea004cced0
Pushkar S <spushkar0071234@gmail.com>
2024-02-17 19:08:00 +0530

    Made the log-in api

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post("/sign-up", async (req, res) => {
         candidatesVoted: []
     };
     
-    console.log("Post request:", data);
+    console.log("Signup request:", req.body);
 
     client = await MongoClient.connect(url);
     try {
@@ -65,6 +65,58 @@ app.post("/sign-up", async (req, res) => {
     }
 });
 
+app.post("/log-in", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let client = null;
+    let dbo = null;
+    let collection = null;
+
+    console.log("Login request:", req.body);
+
+    client = await MongoClient.connect(url);
+    try {
+        dbo = client.db('Voting-System');
+        collection = dbo.collection('Users');
+        const user = await collection.findOne({
+            email: email
+        });
+        if (user) {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                const token = jwt.sign(user, 'secret');
+                await client.close();
+                res.json({
+                    login: true,
+                    token: token,
+                    data: user
+                });
+            }
+            else {
+                await client.close();
+                res.status(400).json({
+                    login: false,
+                    reason: "Wrong password"
+                });
+            }
+        }
+        else {
+            await client.close();
+            res.status(400).json({
+                login: false,
+                reason: "Wrong email or password"
+            });
+        }
+    }
+    catch (err) {
+        await client.close();
+        res.status(500).json({
+            login: false,
+            reason: "Unknown"
+        });
+    }
+});
 
 console.log("Server is listening on port 8080");
 app.listen(port);
